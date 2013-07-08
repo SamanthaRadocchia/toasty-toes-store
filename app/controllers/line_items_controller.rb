5f91a7321d5cae4e4799698a2390c30a30e07ade
SamanthaRadocchia <samantha.radocchia@gmail.com>
2013-07-08 06:49:41 -0400

    added functionality to remove individual products from cart, when product is removed the confirmation modal displays relevant product, formatted cart buttons into table

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -84,13 +84,24 @@ class LineItemsController < ApplicationController
 
   # DELETE /line_items/1
   # DELETE /line_items/1.json
+  # def destroy
+  #   @line_item = LineItem.find(params[:id])
+  #   @line_item.destroy
+
+  #   respond_to do |format|
+  #     format.html { redirect_to line_items_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
+
   def destroy
     @line_item = LineItem.find(params[:id])
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to current_cart }
       format.json { head :no_content }
     end
   end
+
 end
