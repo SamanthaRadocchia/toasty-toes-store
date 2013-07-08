5bf3dfaa04e41c532bdc6aca9787473e6709e068
SamanthaRadocchia <samantha.radocchia@gmail.com>
2013-07-08 03:00:34 -0400

    initial model for a product, as well as a complete maintenance application for this data provided by rails scaffolding, set up validation.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  
+  # VERIFY THAT ALL TEXT FIELDS CONTAIN SOMETHING - use presence 
+	validates :title, :description, :image_url, presence: true
+
+	# VERIFY THAT PRICE IS VALID NUMBER - use numericality and :greater_than_or_equal_to option a value of 0.01
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+	# VERIFY THAT EACH PRODUCT HAS UNIQUE TITLE - use uniqueness 
+	validates :title, uniqueness: true
+
+	# VERIFY THAT URL ENTERED FOR THE IMAGE IS VALID - use format 
+	validates :image_url, allow_blank: true, format: { 
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
+
 end
